7b15d57f62b93ef2a36c7d5df2240822754a629e
smallSohoSolo <b3069741@126.com>
2021-06-16 11:58:46 +0800

    添加上传脚本

--- cmd/build.sh
+++ cmd/build.sh
@@ -1,2 +1,3 @@
-GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o kada-account main/main.go
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o kada-account ../main/main.go
 scp ./kada-account root@39.106.207.62:/root
+rm kada-account
