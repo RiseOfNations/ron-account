aa430fa15b1603135753a30dc309d9b97d39922e
smallSohoSolo <b3069741@126.com>
2021-06-28 16:01:02 +0800

    添加token

--- token/middleware.go
+++ token/middleware.go
@@ -10,14 +10,21 @@ func Middleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		const BearerSchema = "Bearer "
 		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusBadRequest, util.GetNetError("without token", nil))
+			c.Abort()
+			return
+		}
 		accessToken := authHeader[len(BearerSchema):]
 		if accessToken == "" {
 			c.JSON(http.StatusBadRequest, util.GetNetError("auth net", nil))
 			c.Abort()
+			return
 		} else {
 			if VerifyToken(accessToken) == false {
 				c.JSON(http.StatusUnauthorized, util.GetNetError("token net", nil))
 				c.Abort()
+				return
 			}
 		}
 	}
